624e972c59e6c3d77e483393279cef92ed24a930
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-08-25 01:46:37 +0300

    Update pizzaAssort.js

--- src/data/pizzaAssort.js
+++ src/data/pizzaAssort.js
@@ -1,43 +1,43 @@
 export const pizzaList = [
   {
     id: 1,
-    name: "Pizza with Cheese and Basil",
+    name: 'Pizza with Cheese and Basil',
     price: {
       standard: 295,
       large: 342,
     },
-    image: "pizza-1.png",
-    ingredients: ["ingredient 1", "ingredient 2", "ingredient 3"]
+    image: 'pizza1',
+    ingredients: ['ingredient 1', 'ingredient 2', 'ingredient 3'],
   },
   {
     id: 2,
-    name: "Supreme",
+    name: 'Supreme',
     price: {
       standard: 290,
       large: 338,
     },
-    image: "pizza-2.png",
-    ingredients: ["ingredient 1", "ingredient 2", "ingredient 3"]
+    image: 'pizza2',
+    ingredients: ['ingredient 1', 'ingredient 2', 'ingredient 3'],
   },
   {
     id: 3,
-    name: "Margherita Pizza",
+    name: 'Margherita Pizza',
     price: {
       standard: 300,
       large: 345,
     },
-    image: "pizza-3.png",
-    ingredients: ["ingredient 1", "ingredient 2", "ingredient 3"]
+    image: 'pizza3',
+    ingredients: ['ingredient 1', 'ingredient 2', 'ingredient 3'],
   },
   {
     id: 4,
-    name: "Pizza with mashrooms and tomatoes",
+    name: 'Pizza with mashrooms and tomatoes',
     price: {
       standard: 300,
       large: 343,
     },
-    image: "pizza-4.png",
-    ingredients: ["ingredient 1", "ingredient 2", "ingredient 3"]
+    image: 'pizza4',
+    ingredients: ['ingredient 1', 'ingredient 2', 'ingredient 3'],
   },
   {
     id: 5,
@@ -46,77 +46,77 @@ export const pizzaList = [
       standard: 231,
       large: 274,
     },
-    image: "pizza-5.png",
-    ingredients: ["ingredient 1", "ingredient 2", "ingredient 3"]
+    image: 'pizza5',
+    ingredients: ['ingredient 1', 'ingredient 2', 'ingredient 3'],
   },
   {
     id: 6,
-    name: "pizza 6",
+    name: 'pizza 6',
     price: {
       standard: 231,
       large: 274,
     },
-    image: "pizza-6.png",
-    ingredients: ["ingredient 1", "ingredient 2", "ingredient 3"]
+    image: 'pizza6',
+    ingredients: ['ingredient 1', 'ingredient 2', 'ingredient 3'],
   },
   {
     id: 7,
-    name: "White Pizza (Ricotta and Mozzarella)",
+    name: 'White Pizza (Ricotta and Mozzarella)',
     price: {
       standard: 266,
       large: 316,
     },
-    image: "pizza-7.png",
-    ingredients: ["ingredient 1", "ingredient 2", "ingredient 3"]
+    image: 'pizza7',
+    ingredients: ['ingredient 1', 'ingredient 2', 'ingredient 3'],
   },
   {
     id: 8,
-    name: "Mushroom and Olive",
+    name: 'Mushroom and Olive',
     price: {
       standard: 286,
       large: 336,
     },
-    image: "pizza-8.png",
-    ingredients: ["ingredient 1", "ingredient 2", "ingredient 3"]
+    image: 'pizza8',
+    ingredients: ['ingredient 1', 'ingredient 2', 'ingredient 3'],
   },
   {
     id: 9,
-    name: "pizza 9",
+    name: 'pizza 9',
     price: {
       standard: 296,
       large: 339,
     },
-    image: "pizza-9.png",
-    ingredients: ["ingredient 1", "ingredient 2", "ingredient 3"]
+    image: 'pizza9',
+    ingredients: ['ingredient 1', 'ingredient 2', 'ingredient 3'],
   },
   {
     id: 10,
-    name: "Homemade Pepperoni Pizza",
+    name: 'Homemade Pepperoni Pizza',
     price: {
       standard: 268,
       large: 319,
     },
-    image: "pizza-10.png",
-    ingredients: ["ingredient 1", "ingredient 2", "ingredient 3"]
+    image: 'pizza10',
+    ingredients: ['ingredient 1', 'ingredient 2', 'ingredient 3'],
   },
   {
     id: 11,
-    name: "Pizza With Mushrooms",
+    name: 'Pizza With Mushrooms',
     price: {
       standard: 290,
       large: 338,
     },
-    image: "pizza-11.png",
-    ingredients: ["ingredient 1", "ingredient 2", "ingredient 3"]
+    image: 'pizza11',
+    ingredients: ['ingredient 1', 'ingredient 2', 'ingredient 3'],
   },
   {
     id: 12,
-    name: "Mascarpone And Mushrooms",
+    name: 'Mascarpone And Mushrooms',
     price: {
       standard: 279,
       large: 323,
     },
-    image: "pizza-12.png",
-    ingredients: ["ingredient 1", "ingredient 2", "ingredient 3"]
+    image: 'pizza12',
+    ingredients: ['ingredient 1', 'ingredient 2', 'ingredient 3'],
   },
-]
+];
