d0a7a2b06639d43024274fdab2e27364209e66c8
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-08-24 17:26:17 +0300

    Update renderMenu.js

--- src/js/renderMenu.js
+++ src/js/renderMenu.js
@@ -1,55 +1,81 @@
-import { pizzas } from '../data/pizzaAssort'
+import { pizzaList } from '../data/pizzaAssort'
 
-// Render main menu
-const products = pizzas
-let path = "./img/";
+const pickSize = (id, size, price) => {
+  const pickSizeBtns = document.querySelectorAll(".card-btn-group");
+  const priceElements = document.querySelectorAll(".card-price span");
 
-products.forEach(item => {
-  const menuBox = document.querySelector(".menu-card-list");
-  const newLi = document.createElement("li");
-  const { id, name, price, image, ingredients } = item;
-
-  let standardSize = price.standard;
-  let largeSize = price.large;
-  let currentPrice = standardSize;
-  let currentID = id + currentPrice;
-  console.log();
-
-  newLi.classList.add("card-item");
-  newLi.innerHTML = `
-      <div class="card-price"><span>${currentPrice}</span> UAH</div>
-      <img class="card-image" src="${path + image}" alt="${name}">
-      <h4 class="card-title">${name}</h4>
-      <p class="card-text">${ingredients.join(", ")}</p>
-      <div class="card-btn-group btn-group-${id}" role="group" aria-label="Basic example">
-        <button type="button" class="size-btn btn-secondary active" value="standard" onclick="chooseSize('standard', ${id}, ${standardSize}, ${largeSize})" id="${id}-${standardSize}">Standard size</button>
-        <button type="button" class="size-btn btn-secondary" value="large" onclick="chooseSize('large', ${id}, ${standardSize}, ${largeSize})" id="${id}-${largeSize}">Large</button>
-      </div>
-      <input type="button" onclick="addToCart(${id})" value="add to cart" name="add_to_cart" class="add-to-cart-btn">
-    `;
-  menuBox.appendChild(newLi);
-});
-
-function chooseSize(action, id, standardSize, largeSize) {
-  const chooseSizeBtns = document.querySelectorAll(".btn-group");
-  const priceElements = document.querySelectorAll(".price span");
-
-  if (action === "standard" || action === "") {
-    currentPrice = standardSize;
-    chooseSizeBtns[id - 1].firstElementChild.classList.add("active");
-    chooseSizeBtns[id - 1].lastElementChild.classList.remove("active");
-    priceElements[id - 1].innerHTML = currentPrice;
-
-    return currentPrice;
+  if (size === "standard") {
+    pickSizeBtns[id].firstElementChild.classList.add("active");
+    pickSizeBtns[id].lastElementChild.classList.remove("active");
+    priceElements[id].innerHTML = price;
   }
-  if (action === "large") {
-    currentPrice = largeSize;
-    chooseSizeBtns[id - 1].lastElementChild.classList.add("active");
-    chooseSizeBtns[id - 1].firstElementChild.classList.remove("active");
-    priceElements[id - 1].innerHTML = currentPrice;
 
-    return currentPrice;
+  if (size === "large") {
+    pickSizeBtns[id].lastElementChild.classList.add("active");
+    pickSizeBtns[id].firstElementChild.classList.remove("active");
+    priceElements[id].innerHTML = price;
   }
 
   renderMenu();
 }
+
+// Render main menu
+const renderMenu = () => {
+  const path = "./img/";
+
+  pizzas.forEach(pizza => {
+    const menuBox = document.querySelector(".menu-card-list");
+    const newLi = document.createElement("li");
+    const { id, name, price: { standard, large }, image, ingredients } = pizza;
+    let currentPrice = standard;
+
+    newLi.classList.add("card-item");
+    newLi.innerHTML = `
+        <div class="card-price"><span>${currentPrice}</span> UAH</div>
+        <img class="card-image" src="${path + image}" alt="${name}">
+        <h3 class="card-title">${name}</h3>
+        <p class="card-text">${ingredients.join(", ")}</p>
+        <div class="card-btn-group btn-group-${id}">
+          <button
+            type="button"
+            class="size-btn active"
+            data-size="standard"
+            data-price="${standard}"
+            data-id="${id}"
+            id="${id}-${standard}"
+          >
+            Standard size
+          </button>
+
+          <button
+            type="button"
+            class="size-btn"
+            data-size="large"
+            data-price="${large}"
+            data-id="${id}"
+            id="${id}-${large}"
+          >
+            Large
+          </button>
+        </div>
+        <input type="button" onclick="addToCart(${id})" value="add to cart" name="add_to_cart" class="add-to-cart-btn">
+      `;
+
+    menuBox.appendChild(newLi);
+  });
+}
+
+const pizzas = [...pizzaList];
+
+renderMenu();
+
+(() => {
+  const buttons = document.querySelectorAll('.menu .size-btn');
+  buttons.forEach(button => {
+    const size = button.dataset.size;
+    const price = button.dataset.price;
+    const id = button.dataset.id;
+
+    button.addEventListener('click', () => pickSize(id - 1, size, price))
+  })
+})();
