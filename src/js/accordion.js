6389103dc1434d4b0e2cf4f79fe0e842f9277407
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-09-10 20:45:32 +0300

    Update accordion.js

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,32 +1,30 @@
-(() => {
-  const accordionContent = document.querySelectorAll('.accordion-content');
+const accordionContent = document.querySelectorAll('.accordion-content');
 
-  const removeOpen = index => {
-    accordionContent.forEach((item, i) => {
-      if (index !== i) {
-        const list = item.querySelector('.accordion-list');
-
-        item.classList.remove('open');
-        list.style.height = '0px';
-      }
-    });
-  };
-
-  accordionContent.forEach((item, index) => {
-    const header = item.querySelector('.accordion-title');
-
-    header.addEventListener('click', () => {
+const removeOpen = index => {
+  accordionContent.forEach((item, i) => {
+    if (index !== i) {
       const list = item.querySelector('.accordion-list');
 
-      item.classList.toggle('open');
-
-      if (item.classList.contains('open')) {
-        list.style.height = `${list.scrollHeight}px`;
-      } else {
-        list.style.height = '0px';
-      }
-
-      removeOpen(index);
-    });
+      item.classList.remove('open');
+      list.style.height = '0px';
+    }
   });
-})();
+};
+
+accordionContent.forEach((item, index) => {
+  const header = item.querySelector('.accordion-title');
+
+  header.addEventListener('click', () => {
+    const list = item.querySelector('.accordion-list');
+
+    item.classList.toggle('open');
+
+    if (item.classList.contains('open')) {
+      list.style.height = `${list.scrollHeight}px`;
+    } else {
+      list.style.height = '0px';
+    }
+
+    removeOpen(index);
+  });
+});
