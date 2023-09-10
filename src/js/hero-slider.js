f0f0f5bbb5e156df0c70c931f12034f74be87b38
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-09-10 17:03:29 +0300

    prettier

--- src/js/hero-slider.js
+++ src/js/hero-slider.js
@@ -1,13 +1,13 @@
 (() => {
-  const slides = document.querySelectorAll(".swiper-slide");
-  const prevSlideBtns = document.querySelectorAll(".swiper-button-prev");
-  const nextSlideBtns = document.querySelectorAll(".swiper-button-next");
+  const slides = document.querySelectorAll('.swiper-slide');
+  const prevSlideBtns = document.querySelectorAll('.swiper-button-prev');
+  const nextSlideBtns = document.querySelectorAll('.swiper-button-next');
 
-  prevSlideBtns.forEach((btn) => {
-    btn.addEventListener("click", prev);
+  prevSlideBtns.forEach(btn => {
+    btn.addEventListener('click', prev);
   });
-  nextSlideBtns.forEach((btn) => {
-    btn.addEventListener("click", next);
+  nextSlideBtns.forEach(btn => {
+    btn.addEventListener('click', next);
   });
 
   setInterval(() => {
@@ -17,12 +17,12 @@
   function prev() {
     let lastIndex = slides.length - 1;
     for (let i = 0; i < slides.length; i++) {
-      if (slides[i].classList.contains("active")) {
-        slides[i].classList.remove("active");
+      if (slides[i].classList.contains('active')) {
+        slides[i].classList.remove('active');
         if (i !== 0) {
-          slides[i - 1].classList.add("active");
+          slides[i - 1].classList.add('active');
         } else {
-          slides[lastIndex].classList.add("active");
+          slides[lastIndex].classList.add('active');
         }
 
         return;
@@ -33,12 +33,12 @@
   function next() {
     let lastIndex = slides.length - 1;
     for (let i = 0; i < slides.length; i++) {
-      if (slides[i].classList.contains("active")) {
-        slides[i].classList.remove("active");
+      if (slides[i].classList.contains('active')) {
+        slides[i].classList.remove('active');
         if (i !== lastIndex) {
-          slides[i + 1].classList.add("active");
+          slides[i + 1].classList.add('active');
         } else {
-          slides[0].classList.add("active");
+          slides[0].classList.add('active');
         }
 
         return;
